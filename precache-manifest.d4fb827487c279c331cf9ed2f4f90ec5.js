68e5e365874036f24ebc7c39a24c5da78085ea3f
noumanniazi <noumanaamirkhan@gmail.com>
2019-05-18 01:03:10 +0500

    Updates

---
+++ precache-manifest.d4fb827487c279c331cf9ed2f4f90ec5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "35a3db4b0fab84d8b2ae1c0a756bd2e2",
+    "url": "/DragAndDrop/static/media/trello.35a3db4b.png"
+  },
+  {
+    "revision": "2f9564f2c4e0b9663885",
+    "url": "/DragAndDrop/static/js/runtime~main.2f9564f2.js"
+  },
+  {
+    "revision": "8ea6b429c72f7a25a005",
+    "url": "/DragAndDrop/static/js/main.8ea6b429.chunk.js"
+  },
+  {
+    "revision": "1360a20fa313313ced53",
+    "url": "/DragAndDrop/static/js/2.1360a20f.chunk.js"
+  },
+  {
+    "revision": "8ea6b429c72f7a25a005",
+    "url": "/DragAndDrop/static/css/main.528b865d.chunk.css"
+  },
+  {
+    "revision": "1360a20fa313313ced53",
+    "url": "/DragAndDrop/static/css/2.cb282d89.chunk.css"
+  },
+  {
+    "revision": "6d690b48c744b9445942d0bc0c00c08a",
+    "url": "/DragAndDrop/index.html"
+  }
+];
